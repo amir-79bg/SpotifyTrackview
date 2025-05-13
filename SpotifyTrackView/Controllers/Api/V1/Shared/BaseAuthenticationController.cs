fde73d446dbbc4e4a7b7c49a6d14c2fb9dc626b6
realpvz <hellopvz@gmail.com>
2025-05-13 20:06:47 +0330

    fix: token in sign up and fix an email have only a type of user

--- SpotifyTrackView/Controllers/Api/V1/Shared/BaseAuthenticationController.cs
+++ SpotifyTrackView/Controllers/Api/V1/Shared/BaseAuthenticationController.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpotifyTrackView.Data;
 using SpotifyTrackView.DataTransferObjects.Authentication;
+using SpotifyTrackView.Entity;
 using SpotifyTrackView.Interfaces;
+using SpotifyTrackView.Resources.Shared;
 
 namespace SpotifyTrackView.Controllers.Api.V1.Shared;
 
 [ApiController]
-public abstract class BaseAuthenticationController<TUser> : ControllerBase where TUser : class, IAppUser, new()
+public abstract class BaseAuthenticationController<TUser> : BaseApiController where TUser : class, IAppUser, new()
 {
     private readonly DbSet<TUser> _dbSet;
     private readonly IAuthService<TUser> _authService;
@@ -41,15 +43,38 @@ public abstract class BaseAuthenticationController<TUser> : ControllerBase where
     }
 
     [HttpPost("register")]
-    public virtual async Task<TUser> Register([FromBody] RegisterDto registerDto)
+    public virtual async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
     {
+        bool emailExists = await _context.Influencers.AnyAsync(x => x.Email == registerDto.Email)
+                           || await _context.Artists.AnyAsync(x => x.Email == registerDto.Email)
+                           || await _context.Listeners.AnyAsync(x => x.Email == registerDto.Email);
+        
+        if (emailExists)
+        {
+            var errors = new Dictionary<string, string[]>
+            {
+                { "Email", new[] { "This email is already registered under another user type." } }
+            };
+
+            return ValidationProblem(new ValidationProblemDetails(errors));
+        }
+
         TUser appUser = new TUser();
         appUser.Email = registerDto.Email;
         appUser.Password = _authService.HashPassword(appUser, registerDto.Password);
 
         _dbSet.Add(appUser);
         await _context.SaveChangesAsync();
+        
+        List<Claim> claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, appUser.Id.ToString()),
+            new Claim(ClaimTypes.Email, appUser.Email),
+            new Claim(ClaimTypes.Role, typeof(TUser).Name),
+        };
 
-        return appUser;
+        var token = _authService.GenerateJwtToken(appUser, claims);
+        
+        return Ok(AuthResource.From((AppUser)(object) appUser, token));
     }
 }
---
+++ SpotifyTrackView/Resources/Shared/AuthResource.cs
@@ -0,0 +1,23 @@
+﻿using SpotifyTrackView.Entity;
+
+namespace SpotifyTrackView.Resources.Shared;
+
+public class AuthResource
+{
+    public static object From(AppUser user, string token)
+    {
+        return new
+        {
+            user.Id,
+            user.Email,
+            user.FirstName,
+            user.LastName,
+            Token = token,
+            user.Bio,
+            user.Country,
+            user.Region,
+            user.CreatedAt,
+            user.UpdatedAt
+        };
+    }
+}
